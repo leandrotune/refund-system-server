cc828a5b48b372377e46794fe0d08f3092f0b658
leandrotune <leandro.tune@hotmail.com>
2025-05-02 05:23:22 -0300

    fix: category field in the table

--- src/routes/create-Refund.ts
+++ src/routes/create-Refund.ts
@@ -3,22 +3,30 @@ import z from "zod";
 import { prisma } from "../lib/prisma";
 
 export function createRefund(app: FastifyInstance) {
+	const refundCategory = [
+		"FOOD",
+		"TRANSPOR",
+		"HOSTING",
+		"SERVICES",
+		"OTHERS",
+	] as const;
+
 	const createRefundSchema = z.object({
 		personName: z.string().min(1, "Nome da solicitação não preenchido!"),
-		category: z.string().min(1, "selecione uma Categoria"),
+		category: z.enum(refundCategory),
 		amount: z.number().min(1, "informe o valor da solicitação!"),
 		nameFile: z.string(),
 	});
 
 	app.post("/create-refund", async (req, res) => {
-		const { personName, category, amount, nameFile } = createRefundSchema.parse(
+		const { personName, amount, category, nameFile } = createRefundSchema.parse(
 			req.body,
 		);
 
 		const refund = await prisma.refund.create({
 			data: {
 				personName,
-				category: "FOOD",
+				category: category,
 				amount,
 				nameFile,
 			},
